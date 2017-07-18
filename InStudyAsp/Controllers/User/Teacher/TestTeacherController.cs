a105538faf84ca3b9624e8d10d2824859431114f
Paul <b.paul4@gmail.com>
2017-07-18 11:49:41 +0300

    Were added follders for teacher project segment

---
+++ InStudyAsp/Controllers/User/Teacher/TestTeacherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InStudyAsp.Controllers.User.Teacher
+{
+    public class TestTeacherController : Controller
+    {
+        // GET: TestTeacher
+        public ActionResult Index()
+        {
+            return View("../User/Teacher/TestTeacher");
+        }
+    }
+}
